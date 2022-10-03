9ff3aa5d5369a1fcbe5c2803d9b652ff88db48d6
Joran <joran.manoel@hotmail.com>
2022-10-03 12:51:39 -0300

    Update stryker.conf.mjs
    Add variables on line 11 to 14

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -9,5 +9,10 @@ const config = {
   reporters: ["html", "clear-text", "progress", "dashboard"],
   testRunner: "jest",
   coverageAnalysis: "perTest",
+  
+  BASE_URL: "https://dashboard.stryker-mutator.io for production, https://stryker-dashboard-acceptance.azurewebsites.net/" ,
+  PROJECT: "https://github.com/JoranManoel/mutation-test/",
+  VERSION: "main",
+  API_KEY: "f023902a-28d8-4863-bef3-72402f010a8d",
 };
 export default config;
